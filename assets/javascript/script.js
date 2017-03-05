fd9dadb7bbadfbcd3466ffd30ff97ebea09fc730
Rukaroa <rukaroa@gmail.com>
2017-03-04 21:38:07 -0600

    html skeleton

---
+++ assets/javascript/script.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyBynl4WWMWNB9emOAo3R_4ewi6Sr5yk91s",
+	authDomain: "rukatrains.firebaseapp.com",
+	databaseURL: "https://rukatrains.firebaseio.com",
+	storageBucket: "rukatrains.appspot.com",
+	messagingSenderId: "42620412251"
+};
+firebase.initializeApp(config);
+
+
