1db5b11074607a08068a8802e4feddf67947b3da
Rukaroa <rukaroa@gmail.com>
2017-03-05 21:06:54 -0600

    submit function started

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -8,4 +8,50 @@ var config = {
 };
 firebase.initializeApp(config);
 
+var database = firebase.database();
 
+//pass values to database on click
+$("#submit-btn").on("click", function(event){
+	event.preventDefault();
+
+	var trainName = $("#train-name").val().trim();
+	var destination = $("#destination").val().trim();
+	var firstTime = $("#first-time").val().trim();
+	var freq = $("#frequency").val().trim();
+
+	var arrival;
+	var minutesAway;
+
+	var array = [
+		trainName,
+		destination,
+		firstTime,
+		freq,
+		arrival,
+		minutesAway
+	];
+
+	var length = arrival.length;
+	var newRow = $("<tr>");
+
+	for (var i = 0; i < length; i++) {
+		var newCol = $("<td>");
+		newCol.text(array[i]);
+		newRow.append(newCol);
+	}
+
+	//firebase push data
+	database.ref().push({
+		trainName: trainName,
+		destination: destination,
+		firstTime: firstTime,
+		freq: frequency,
+		arrival: arrival,
+		minutes: minutesAway
+	});
+});
+
+//call values from database and add to table on push
+database.ref().on("child_added", function(snapshot){
+
+});
